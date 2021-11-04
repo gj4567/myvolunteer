e4df3355bb4d85f18b57be650bb16d055bdbbf24
gj4567 <gj4567@naver.com>
2021-11-04 16:06:27 +0900

    init

---
+++ src/main/java/org/iot/myvolunteer/MyvolunteerApplication.java
@@ -0,0 +1,13 @@
+package org.iot.myvolunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvolunteerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iot/myvolunteer/MyvolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iot.myvolunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
